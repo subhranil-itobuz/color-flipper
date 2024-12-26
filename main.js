19b3ddb6fad86034f01a89390ff3ebe50b56f8ba
Subhranil Das <subhranil@itobuz.com>
2024-12-26 18:05:33 +0530

    Color flipper done

---
+++ main.js
@@ -0,0 +1,15 @@
+function aquaColor() {
+  document.body.style.backgroundColor = "aqua";
+}
+
+function yellowColor() {
+  document.body.style.backgroundColor = "yellow";
+}
+
+function greenColor() {
+  document.body.style.backgroundColor = "green";
+}
+
+function grayColor() {
+  document.body.style.backgroundColor = "gray";
+}
